c0738f2a7c271fc2535b47c76b145ab83e3e7672
sirfreedom <alemillan22@gmail.com>
2024-05-13 16:00:14 +0200

    Update de 17 a 18 version de react final.
    Update de 17 a 18 version de react final.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createRoot } from 'react-dom/client';
-
 
 const container = document.getElementById('root');
-const root = createRoot(container); 
+const root = ReactDOM.createRoot(container); 
 
 root.render( 
 
 <React.StrictMode>
   <App />
-</React.StrictMode>,
+</React.StrictMode>
 
 );
 
