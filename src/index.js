1c96eb500486d4e3f20c9e0cd36e7d789dd177b0
sirfreedom <alemillan22@gmail.com>
2024-05-13 21:44:59 +0200

    Downgrade to 17
    Downgrade to 17

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container); 
+import ReactDOM from 'react-dom';
 
-root.render( 
 
-<React.StrictMode>
-  <App />
-</React.StrictMode>
+//const container = document.getElementById('root');
+//const root = ReactDOM.createRoot(container); 
 
-);
+
+ReactDOM.render( <App />,  document.getElementById('root') );
+
+
+//root.render( <React.StrictMode> <App /> </React.StrictMode> );
 
 
 
